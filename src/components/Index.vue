18f016f5586d7b7d103a53ae93cc6623339cbd9e
root <alexbzg@gmail.com>
2018-08-21 17:20:29 +0300

    -

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="list list_small">
+        index
+    </div>
+   
+</template>
+
+<script>
+// import api from '../api'
+
+export default {
+  name: 'index',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
