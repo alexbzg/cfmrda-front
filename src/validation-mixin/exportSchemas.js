9b23293cc5425a927393a30f2bb6a3abd843130b
root <alexbzg@gmail.com>
2018-10-19 17:51:33 +0300

    -

--- src/validation-mixin/exportSchemas.js
+++ src/validation-mixin/exportSchemas.js
@@ -1,10 +1,3 @@
-import fs from 'fs'
 import createSchemas from './createSchemas'
 
-if (process.argv.length > 2) {
-  const schemas = JSON.stringify(createSchemas())
-  fs.writeFile(process.argv[2], schemas, (err) => {
-    if (err) throw err
-    console.log('OK')
-  })
-}
+console.log(JSON.stringify(createSchemas()))
