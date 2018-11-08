550fd9b39085a96fe70e72584915ade2bf87a057
root <alexbzg@gmail.com>
2018-11-08 15:30:36 +0300

    -

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+chown web_ftp:www-data src -R
+rm /var/www/cfmrda/js/*
+cp /var/www/cfmrda-dev/dist/js/* /var/www/cfmrda/js/
+rm /var/www/cfmrda/css/*
+cp /var/www/cfmrda-dev/dist/css/* /var/www/cfmrda/css/
+cp /var/www/cfmrda-dev/dist/images/* /var/www/cfmrda/images
+cp /var/www/cfmrda-dev/dist/html/* /var/www/cfmrda/html
+cp /var/www/cfmrda-dev/dist/*.html /var/www/cfmrda/
+cp /var/www/cfmrda-dev/dist/favicon.ico /var/www/cfmrda/
