617cd4c1dc30f2392ce42668c7fc1f3ce0631738
root <alexbzg@gmail.com>
2018-11-29 17:13:41 +0300

    -

---
+++ src/components/BandSelect.vue
@@ -0,0 +1,22 @@
+<template>
+    <select :value="value" @input="bandChange($event)">
+        <option v-for="(band, idx) in $options.BANDS" :key="idx">{{band}}</option>
+    </select>
+</template>
+<script>
+import {orderedBands} from '../ham-radio'
+
+export default {
+  BANDS: orderedBands(),
+  name: 'BandSelect',
+  props: ['value'],
+  data () {
+    return {}
+  },
+  methods: {
+    bandChange (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
--- src/components/PaperCFM.vue
+++ src/components/PaperCFM.vue
@@ -1,100 +1,110 @@
 <template>
-    <div>
-        <vue-recaptcha v-if="!userToken"
-            ref="recaptcha"
-            @verify="onRecaptchaVerify"
-            @expired="onRecaptchaExpired"
-            size="invisible"
-            :sitekey="$options.RECAPTCHA_SITE_KEY">
-        </vue-recaptcha>
-
-        <p>Данные QSO будут отправлены корреспонденту по email, указанному им в профиле QRZ.com.<br/>Если корреспондент подтвердит QSO с вами, то подтвержденное QSO будет добавлено в базу CFMRDA.ru.</p>
-        <p>The QSO data will be sent to the correspondent by the email specified by him in the QRZ.com profile.<br/>If the correspondent will confirm QSO with you the confirmed QSO will be added to the database of CFMRDA.ru.</p>
-        <table id="cfm_qso_request">
-            <tr>
-                <td colspan="10" class="no_border">
-                    <input type="text" name="rda_callsign" id="rda_callsign" 
-                        v-model="request.email" :class="{error: validationErrors.email}"/>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="10" class="rda_callsign_note">Ваш email - Your email</td>
-            </tr>
-            <tr>
-                <td class="menu rda_callsign">Station callsign</td>
-                <td class="menu rda">RDA</td>
-                <td class="menu date">Date</td>
-                <td class="menu time">GMT</td>
-                <td class="menu band">MHz</td>
-                <td class="menu mode">Mode</td>
-                <td class="menu my_callsign">Your callsign</td>
-                <td class="menu rda_rst">Snt RST</td>
-                <td class="menu my_rst">Rcv RST</td>
-                <td class="menu del"></td>
-            </tr>
-            <tr v-for="(qso, idx) in request.qso" :key="idx">
-                <td class="rda_callsign">
-                    <input type="text" name="qso_rda_callsign" id="qso_rda_callsign"
-                        v-model="qso.callsign" 
-                        :class="{error:validationErrors['qso.' + idx + '.callsign']}">
-                </td>
-                <td class="rda">
-                    <input type="text" name="qso_cfm_rda" id="qso_cfm_rda" v-model="qso.rda"
-                        :class="{error:validationErrors['qso.' + idx + '.rda']}">
-                </td>
-                <td class="date">
-                    <datepicker v-model="qso.date"></datepicker>
-                </td>
-                <td class="time">
-                    <input type="text" name="qso_time" id="qso_time" v-model="qso.time">
-                </td>
-                <td class="band">
-                    <select v-model="qso.band" :class="{error: !qso.band}">
-                        <option v-for="(band, idx) in $options.BANDS" :key="idx">{{band}}</option>
-                    </select>
-                </td>
-                <td class="mode">
-                    <select v-model="qso.mode" :class="{error: !qso.mode}">
-                        <option v-for="(mode, idx) in $options.MODES" :key="idx">{{mode}}</option>
-                    </select>
-                </td>
-                <td class="my_callsign">
-                    <input type="text" name="qso_my_callsign" id="qso_my_callsign"
-                        v-model="qso.stationCallsign" 
-                        :class="{error: validationErrors['qso.' + idx + '.stationCallsign']}">
-                </td>
-                <td class="rda_rst">
-                    <input type="text" name="qso_dx_rst" id="qso_dx_rst"
-                        v-model="qso.sentRST">
-                </td>
-                <td class="my_rst">
-                    <input type="text" name="qso_my_rst" id="qso_my_rst"
-                        v-model="qso.recRST">
-                </td>
-                <td class="del">
-                    <img src="images/icon_delete.png" title="Удалить эту строку - Delete this line"
-                        @click="deleteQso(idx)">
-                </td>
-            </tr>
-            <tr>
-                <td colspan="10" class="add_line" @click="addQso()">
-                    Добавить ещё одну строку - Add one more line
-                </td>
-            </tr>
-            <tr>
-                <td colspan="10" class="send_btn">
-                    <input type="button" name="send_btn" id="send_btn" 
-                        value="Отправить запрос DX1DX на подтверждение QSO - 
-                            Send the request to DX1DX for CFM QSO"
-                        class="btn" :disabled="pending || !validated" 
-                        @click="sendClick()">
-                </td>
-            </tr>
-                
-        </table>
-       
+    <div id="paper_qsl">
+        <h4>CFM RDA по бумажным QSL-карточкам</h4>
+        <p>Заполните форму ввода данными из полученной QSL-карточки, подтверждающей QSO с необходимым вам RDA. Если QSL карточка выписана вам на старый позывной, то в поле "Your сallsign" укажите его, а в поле "New callsign" можно указать ваш новый позывной, чтобы это QSO пошло в зачёт и новому позывному.<br/>
+        После проверки модератором данные будут добавлены в  базу CFMRDA.</p>
+        <p class="grey_note">Fill in the input form with data from the received QSL card confirming the QSO with the RDA you need. If a QSL card is issued to your old callsign, then enter it in the "QSL callsign" field, and you can enter your new callsign in the "New callsign" field so that this QSO will go on to the new callsign.<br/>
+        Your data will be added to CFMRDA database after the check by the moderator.</p>
+    <table id="add_paper_qsl">
+    <tr>
+        <td class="top qsl_callsign">Station's callsign</td>
+        <td class="top qsl_rda">RDA</td>
+        <td class="top qsl_date">Date</td>
+        <td class="top qsl_time">GMT</td>
+        <td class="top qsl_band">MHz</td>
+        <td class="top qsl_mode">Mode</td>
+        <td class="top qsl_my_call">Your сallsign</td>
+        <td class="top qsl_my_new_call" colspan="2">New callsign</td>
+        <td class="top del"></td>
+    </tr>
+    <tr>
+        <td class="qsl_callsign">
+            <input type="text" name="qsl_callsign" id="qsl_callsign" v-model="qso.callsign">
+        </td>
+        <td class="qsl_rda"><input type="text" name="qsl_rda" id="qsl_rda"></td>
+        <td class="qsl_date">input календарь</td>
+        <td class="qsl_time"><input type="text" name="qso_time" id="qso_time"></td>
+        <td class="qsl_band">
+            <select>
+                <option>1.8</option>
+                <option>3.5</option>
+                <option>7</option>
+                <option>10</option>
+                <option>14</option>
+                <option>18</option>
+                <option>21</option>
+                <option>24</option>
+                <option>28</option>
+            </select>
+        </td>
+        <td class="qsl_mode">
+            <select>
+                <option>CW</option>
+                <option>SSB</option>
+                <option>DIGI</option>
+            </select>
+        </td>
+        <td class="qsl_my_call"><input type="text" name="qsl_my_call" id="qsl_my_call"></td>
+        <td class="qsl_my_new_call" colspan="2"><input type="text" name="qsl_my_new_call" id="qsl_my_new_call"></td>
+        <td rowspan="2" class="del"><input type="button" name="save_qsl" id="save_qsl" value="OK" class="btn"></td>
+    </tr>
+    <tr>
+        <td colspan="9" class="qsl_upload"><b>Загрузите скан или фото QSL-карточки!</b> <input type="file" name="qsl_callsign"></td>
+    </tr>
+    <tr><td colspan="10" class="no_border"></td></tr>
+    <tr>
+        <td class="qsl_callsign">DX1DX</td>
+        <td class="qsl_rda">KK-23</td>
+        <td class="qsl_date">12 Dec 2017</td>
+        <td class="qsl_time">07:15</td>
+        <td class="qsl_band">7</td>
+        <td class="qsl_mode">CW</td>
+        <td class="qsl_my_call">TE1STA</td>
+        <td class="qsl_my_new_call">TE1S</td>
+        <td class="qsl_card"><a href="#"><img src="/images/icon_qsl.png" title="Просмотр QSL"></a></td>
+        <td class="del"><img src="/images/icon_delete.png" title="Удалить эту строку - Delete this line"></td>
+    </tr>
+    <tr>
+        <td class="qsl_callsign">DX2DX/P</td>
+        <td class="qsl_rda">KK-11</td>
+        <td class="qsl_date">15 Mar 2018</td>
+        <td class="qsl_time">08:50</td>
+        <td class="qsl_band">14</td>
+        <td class="qsl_mode">SSB</td>
+        <td class="qsl_my_call">TE1STA</td>
+        <td class="qsl_my_new_call">TE1S</td>
+        <td class="qsl_card"><a href="#"><img src="/images/icon_qsl.png" title="Просмотр QSL"></a></td>
+        <td class="del"><img src="/images/icon_delete.png" title="Удалить эту строку - Delete this line"></td>
+    </tr>
+    <tr class="cfmd">
+        <td class="qsl_callsign">DX3DX/P</td>
+        <td class="qsl_rda">KK-13</td>
+        <td class="qsl_date">15 Mar 2018</td>
+        <td class="qsl_time">08:50</td>
+        <td class="qsl_band">14</td>
+        <td class="qsl_mode">SSB</td>
+        <td class="qsl_my_call">TE1STA</td>
+        <td class="qsl_my_new_call">TE1S</td>
+        <td class="qsl_card"><img src="/images/icon_qsl_cfm.png" title="QSL проверена. Информация добавлена в базу CFMRDA."></td>
+        <td class="del"><img src="/images/icon_delete.png" title="Удалить эту строку - Delete this line"></td>
+    </tr>
+    <tr class="not_cfmd">
+        <td class="qsl_callsign">DX4DX/P</td>
+        <td class="qsl_rda">KK-14</td>
+        <td class="qsl_date">16 Mar 2018</td>
+        <td class="qsl_time">07:50</td>
+        <td class="qsl_band">10</td>
+        <td class="qsl_mode">CW</td>
+        <td class="qsl_my_call">TE1STA</td>
+        <td class="qsl_my_new_call">TE1S</td>
+        <td class="qsl_card"><img src="/images/icon_qsl_not_cfm.png" title="QSL не прошла проверку."><Br/>Не указан RDA</td>
+        <td class="del"><img src="/images/icon_delete.png" title="Удалить эту строку - Delete this line"></td>
+    </tr>
+    </table>
         <div v-if="response" id="message" v-html="response"></div>
+
     </div>
+       
 </template>
 
 <script>
