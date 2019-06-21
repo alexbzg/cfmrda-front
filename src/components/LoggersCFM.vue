ae9e9b60dc9f98349a09c1d9cdef4794f3256f23
root <alexbzg@gmail.com>
2019-06-21 14:46:21 +0300

    -

--- src/components/LoggersCFM.vue
+++ src/components/LoggersCFM.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="elogs_cfm">
     
-        <h4>LoTW/eQSL CFM</h4>
-            <p>Укажите свой логин/пароль доступа к системам LoTW и eQSL.<br/>После проверки по нашей базе RDA подтвержденные QSO будут добавлены к вашей статистике CFMRDA.</p>
-            <p class="grey_note">Enter your login/password for LoTW and eQSL systems.<br/>Verified QSOs will be added to your CFMRDA statistics аfter checking with our RDA database.</p>
+        <h4>LoTW CFM</h4>
+            <p>Укажите свой логин/пароль доступа к системе LoTW.<br/>После нажатия «OK» пройдет проверка связи с сервером LoTW. Если всё прошло хорошо, появится значок <img src="/images/icon_connected.png" width="12">.<br/>CFMD QSO скачаются в течение суток и после проверки по нашей базе RDA будут добавлены к вашей статистике.<br/>В дальнейшем новые CFMD QSO будут скачиваться из LoTW один раз в месяц.</p>
+            <p class="grey_note">Enter your login/password to access the LoTW system. The connection with the LoTW server will be checked after clicking «OK». If everything went well the <img src="/images/icon_connected.png"  width="12"><br/>icon will appear. The confirmed QSOs will be downloaded to CFMRDA within 24 hours and after checking with our RDA database will be added to your statistics.<br/>New CFMD QSOs will be downloaded from LoTW once a month.</p>
 
         <table class="elog_stats">
             <tr>
@@ -58,6 +58,7 @@ export default {
     },
     loggerUpdate (logger, loginData) {
       logger.loginData = loginData
+      logger.state = null
       this.post({
         update: {
           logger: logger.logger,
